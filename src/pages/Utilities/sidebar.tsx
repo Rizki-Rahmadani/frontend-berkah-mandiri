a40814f34342261275d317bec32c5f0a47a0ed5d
RizkiRahmadani <rzkrhmdn7@gmail.com>
2025-05-01 21:34:26 +0700

    core: fix route

--- src/pages/Utilities/sidebar.tsx
+++ src/pages/Utilities/sidebar.tsx
@@ -16,34 +16,48 @@ export const Sidebar = ({ onClose }: { onClose?: () => void }) => {
     >
       <div className="flex flex-col justify-between w-full h-full font-semibold items-start">
         <div className="flex flex-col w-full">
-          <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
-            <LayoutDashboardIcon />
-            <p>Dashboard</p>
-          </div>
-          <Link to={"/pelanggan"}>
+          <Link to={"/"}>
+            <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
+              <LayoutDashboardIcon />
+              <p>Dashboard</p>
+            </div>
+          </Link>
+
+          <Link to={"/pembelian-barang"}>
             <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
               <ShoppingCart />
               <p>Pembelian Barang</p>
             </div>
           </Link>
-          <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
-            <PersonStandingIcon />
-            <p>Pelanggan</p>
-          </div>
-          <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
-            <ListTreeIcon />
-            <p>Riwayat</p>
-          </div>
+
+          <Link to={"/pelanggan"}>
+            <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
+              <PersonStandingIcon />
+              <p>Pelanggan</p>
+            </div>
+          </Link>
+
+          <Link to={"/riwayat-transaksi"}>
+            <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
+              <ListTreeIcon />
+              <p>Riwayat</p>
+            </div>
+          </Link>
         </div>
+
         <div className="flex flex-col w-full">
-          <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
-            <Settings />
-            <p>Pengaturan </p>
-          </div>
-          <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
-            <LogOut />
-            <p>Logout</p>
-          </div>
+          <Link to={"Pengaturan"}>
+            <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
+              <Settings />
+              <p>Pengaturan </p>
+            </div>
+          </Link>
+          <Link to={"/logout"}>
+            <div className="flex gap-3 w-full cursor-pointer xl:text-lg md:text-sm  hover:bg-[#DCD7C9] text-[#DCD7C9] hover:text-[#2C3639] p-2 rounded-md transition-all duration-200 ease-in-out">
+              <LogOut />
+              <p>Logout</p>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
